91e661794fad1d8a58f5fefe0bafcfb65d5b9df0
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-31 08:49:00 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,53 @@
+//Array of objects
+const quiz = [
+    {
+        q:"I wonder ..... mind watching this bag for me for a moment.",
+        options:["wether you would", "if would you", "you would", "would you"],
+        answer: 0
+    },
+    {
+        q:"..... made him them want to buy that car? They must be crazy! It's absolutely hideous!",
+        options:["Wherever", "Whatever", "Whenever", "However"],
+        answer: 1
+    },
+    {
+        q:"She'll find it hard to live ..... to the expectations of her parents.",
+        options:["with", "away", "up", "down"],
+        answer: 2
+    },
+    {
+        q:"I thought the film was ..... brilliant.",
+        options:["very", "a bit", "slightly", "absolutely"],
+        answer: 3
+    },
+    {
+        q:"I know that I will never be ..... .",
+        options:["down and out", "short and sweet", "tried and tested", "born and bred"],
+        answer: 0
+    },
+    {
+        q:"He sold most of his personal ..... in order to raise money for his business.",
+        options:["items", "belongings", "possessives", "ownings"],
+        answer: 1
+    },
+    {
+     q:"She has recently been promoted and she now heads ..... the marketing department.",
+        options:["in", "down", "up", "over"],
+        answer: 2   
+    },
+    {
+        q:"The girl we interviewed yesterday seems to be quite ..... .",
+        options:["determination", "determining", "determine", "determined"],
+        answer: 3
+    },
+    {
+        q:"Don't worry, everyone has their ..... . Thing will get better soon.",
+        options:["ups and downs", "dos and don'ts", "pros and cons", "ins and outs"],
+        answer: 0
+    },
+    {
+        q:"To ..... a budget is to calculate the money you need so that you don't spend too much.",
+        options:["set in", "work out", "put down", "make up"],
+        answer: 1
+    }
+]
